1b53b00d310e72c1b97e9853cbd431d9ac5c4dd0
Trevor Osborne <trevorosborne89@yahoo.com>
2025-01-01 18:31:16 -0800

    returned to localhost environment

--- src/main/java/nchandi/spring/services/NCHANDIWebsite_Service/configs/ServiceCorsFilter.java
+++ src/main/java/nchandi/spring/services/NCHANDIWebsite_Service/configs/ServiceCorsFilter.java
@@ -2,6 +2,7 @@ package nchandi.spring.services.NCHANDIWebsite_Service.configs;
 
 import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
@@ -20,14 +21,15 @@ public class ServiceCorsFilter implements Filter {
   public void init(FilterConfig filterConfig) {
   }
 
+  @Value("${ui.url}")
+  private String uiUrl;
+
   @Override
   public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
       throws IOException, ServletException {
     HttpServletResponse response = (HttpServletResponse) res;
     HttpServletRequest request = (HttpServletRequest) req;
-    // For local testing use below url
-    // response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-    response.setHeader("Access-Control-Allow-Origin", "http://ec2-54-215-137-143.us-west-1.compute.amazonaws.com:3000");
+    response.setHeader("Access-Control-Allow-Origin", uiUrl);
     response.setHeader("Access-Control-Allow-Methods", "*");
     response.setHeader("Access-Control-Max-Age", "3600");
     response.setHeader("Access-Control-Allow-Headers", "*");
